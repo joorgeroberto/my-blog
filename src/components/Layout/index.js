5aaaaa27a816a4a02e2286f62307ef690d054674
Jorge Roberto <jorgercj@dcomp.ufs.br>
2019-09-17 18:06:05 -0300

    Learning Styled Components - GlobalStyles

---
+++ src/components/Layout/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Profile from "../Profile"
+
+import * as S from './styled'
+import GlobalStyles from "../../styles/global"
+
+
+const Index = ({ children }) => {
+  return (
+    <S.LayoutWrapper>
+      <GlobalStyles/>
+      <aside>
+        <Profile />
+      </aside>
+      <S.LayoutMain>{children}</S.LayoutMain>
+
+    </S.LayoutWrapper>
+  )
+}
+
+Index.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Index
