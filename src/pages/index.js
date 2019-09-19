3a171ac52a79b4dfa905496a6c193221ad52689d
Jorge Roberto <jorgercj@dcomp.ufs.br>
2019-09-19 16:53:52 -0300

    Add PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,39 @@ import {Link} from 'gatsby';
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
 
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home</h1>
+    <PostItem
+        slug="/slug/"
+        background="red"
+        category="Misc"
+        date="30 de julho de 2019"
+        timeToRead="5"
+        title="Diga não ao Medium: tenha sua própria plataforma"
+        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
+    <PostItem
+        slug="/slug/"
+        background="blue"
+        category="Misc"
+        date="30 de julho de 2019"
+        timeToRead="5"
+        title="Diga não ao Medium: tenha sua própria plataforma"
+        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
+    <PostItem
+        slug="/slug/"
+        background="yellow"
+        category="Misc"
+        date="30 de julho de 2019"
+        timeToRead="5"
+        title="Diga não ao Medium: tenha sua própria plataforma"
+        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium"
+    />
   </Layout>
 )
 
